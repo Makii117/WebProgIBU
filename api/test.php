e28c65e7aa74ba5f31d079a8688a6ac0128c9218
Makii117 <MakiOnPc@gmail.com>
2021-03-16 22:41:08 +0100

    Testing dao

--- api/test.php
+++ api/test.php
@@ -14,11 +14,13 @@ $user1=[
 "account_id"=>1
 ];
 
+
 $user=$user_dao->add_user($user1);
 
 //$user =$user_dao->get_user_by_id(3);
 
 //$user_dao->get_user_by_email("maki@gmail.com");
+
 print_r($user)
 
 ?>
