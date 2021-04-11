27d2c02df3d1a32775068f554cb7f494ca534872
Makii117 <MakiOnPc@gmail.com>
2021-04-11 20:05:55 +0200

    moved update to basedao

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -20,7 +20,16 @@ class BaseDao{
 public function insert(){
   }
 
-  public function update(){
+  public function update($table,$id,$entity,$id_column="id"){
+    $query="UPDATE ${table} SET ";
+    foreach($entity as $name=>$value){
+            $query .= $name ."= :".$name.", ";
+    }
+    $query=substr($query,-0,-2);
+    $query .= "WHERE id =:id";
+    $stmt=$this->connection->prepare($query);
+    $entity['id']=$id;
+    $stmt->execute($entity);
   }
 
   public function query($query,$params){
