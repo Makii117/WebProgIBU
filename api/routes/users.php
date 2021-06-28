229186f2fbbb035fba5b681d47a2dc226f597d20
Makii117 <MakiOnPc@gmail.com>
2021-06-28 16:08:00 +0200

    Added bootstrap template

--- api/routes/users.php
+++ api/routes/users.php
@@ -1,13 +1,31 @@
 <?php
 
-Flight::route('POST /users/register', function(){
+Flight::route('POST /register', function(){
   $data = Flight::request()->data->getData();
   Flight::json(Flight::userService()->register($data));
 });
 
-Flight::route('GET /users/confirm/@token', function($token){
+Flight::route('GET /confirm/@token', function($token){
   Flight::userService()->confirm($token);
   Flight::json(["message" => "Your account has been activated"]);
+});
+
+
+Flight::route('POST /login', function(){
+  $data = Flight::request()->data->getData();
+  Flight::json(Flight::userService()->login($data));
+});
+
+Flight::route('POST forgot', function(){
+  $data = Flight::request()->data->getData();
+  Flight::userService()->forgot($data);
+  Flight::json(["message" => "Recovery link has been sent to your email"]);
+});
+
+Flight::route('POST /reset', function(){
+  $data = Flight::request()->data->getData();
+  Flight::userService()->reset($data);
+  Flight::json(["message" => "Your password has been changed"]);
 });
 
 ?>
--- api/services/UsersService.class.php
+++ api/services/UsersService.class.php
@@ -3,6 +3,8 @@ require_once dirname(__FILE__). '/BaseService.class.php';
 require_once dirname(__FILE__).'/../dao/UserDao.class.php';
 require_once dirname(__FILE__).'/../dao/AccountDao.class.php';
 
+use \Firebase\JWT\JWT;
+
 class UserService extends BaseService{
 
   private $accountDao;
@@ -11,23 +13,28 @@ class UserService extends BaseService{
     $this->dao = new UserDao();
   }
 
+
+  public function login($user){
+    $db_user = $this->dao->get_user_by_email($user['email']);
+
+    if (!isset($db_user['id'])) throw new Exception("User doesn't exists", 400);
+
+    if ($db_user['password'] != md5($user['password'])) throw new Exception("Invalid password", 400);
+    
+    return $db_user;
+  }
+
   public function register($user){
-    if (!isset($user['account'])) throw new Exception("Account field is required");
+   
     $this->dao->beginTransaction();
     try {
- 
-      $account = $this->accountDao->add([
-        "name" => $user['account'],
-        "status" => "PENDING",
-        "created_at" => date(Config::DATE_FORMAT)
-      ]);
 
       $user = parent::add([
-        "account_id" => $account['id'],
+
         "name" => $user['name'],
         "email" => $user['email'],
         "password" => $user['password'],
-        "status" => "PENDING",
+        "status" => "ACTIVE",
         "role" => "USER",
         "created_at" => date(Config::DATE_FORMAT),
         "token" => md5(random_bytes(16))
@@ -48,16 +55,41 @@ class UserService extends BaseService{
     return $user;
   }
 
+
   public function confirm($token){
     $user = $this->dao->get_user_by_token($token);
 
-    if (!isset($user['id'])) throw Exception("Invalid token");
+    if (!isset($user['id'])) throw Exception("Invalid token",400);
 
-    $this->dao->update($user['id'], ["status" => "ACTIVE"]);
-    $this->accountDao->update($user['account_id'], ["status" => "ACTIVE"]);
-
+    $this->dao->update($user['id'], ["status" => "ACTIVE",'token'=>NULL]);
+    
     //TODO send email to customer
   }
 
-}
+
+  public function reset($user){
+    $db_user = $this->dao->get_user_by_token($user['token']);
+
+    if (!isset($db_user['id'])) throw new Exception("Invalid token", 400);
+    if (strtotime(date(Config::DATE_FORMAT)) - strtotime($db_user['token_created_at']) > 300) throw new Exception("Token expired", 400);
+
+    $this->dao->update($db_user['id'], ['password' => md5($user['password']),'token'=>NULL]);
+    return $db_user;
+  }
+
+
+  public function forgot($user){
+    $db_user = $this->dao->get_user_by_email($user['email']);
+
+    if (!isset($db_user['id'])) throw new Exception("User doesn't exists", 400);
+    if (strtotime(date(Config::DATE_FORMAT)) - strtotime($db_user['token_created_at']) < 300) throw new Exception("Be patient tokens is on his way", 400);
+
+    // generate token - and save it to db
+    $db_user = $this->update($db_user['id'], ['token' => md5(random_bytes(16)), 'token_created_at' => date(Config::DATE_FORMAT)]);
+
+    //send recovery main TODO
+
+  }
+  }
+
 ?>
